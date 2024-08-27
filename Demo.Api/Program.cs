d9a5236d30fa6355f5dcfae691db9f6ad735d1f4
Philippe Waeber <philippe.waeber@galenica.com>
2024-08-27 09:13:14 +0200

    2. Add Serilog - Reference package Serilog.AspNetCore - Adapt application startup in Program.cs to   - Remove logging configuration in appsettings   - Hard-code Serilog logging configuration in Program.cs   - Use Serilog with ILogger<T> (by calling builder.Services.AddSerilog())   - Wrap application startup in try/catch block. Log a fatal message in case of failure

--- Demo.Api/Program.cs
+++ Demo.Api/Program.cs
@@ -1,42 +1,72 @@
-var builder = WebApplication.CreateBuilder(args);
+using Serilog;
+using Serilog.Events;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .MinimumLevel.Override("Microsoft.AspNetCore.Hosting", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.AspNetCore.Routing", LogEventLevel.Warning)
+    .WriteTo.Console()
+    .CreateLogger();
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Log.Information("Start configuring Demo.API");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Services.AddSerilog();
+
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    app.UseSerilogRequestLogging();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    var summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    app.MapGet("/weatherforecast",
+               (ILogger<WeatherForecast> logger) =>
+               {
+                   var forecast = Enumerable.Range(1, 5)
+                       .Select(index =>
+                                   new WeatherForecast(
+                                       DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                                       Random.Shared.Next(-20, 55),
+                                       summaries[Random.Shared.Next(summaries.Length)]
+                                   ))
+                       .ToList();
+
+                   logger.LogDebug("Generated weather forecast: {forecast}", forecast);
+
+                   return forecast;
+               })
+        .WithName("GetWeatherForecast")
+        .WithOpenApi();
+
+    Log.Information("Finished configuring Demo.API. Run it.");
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
+catch (Exception ex)
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
+    Log.Fatal(ex, "Host terminated unexpectedly");
+}
+finally
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
-app.Run();
+    Log.CloseAndFlush();
+}
 
 internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
